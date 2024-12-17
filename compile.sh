7a48dc7e83dcc5ac1766aaad0ff5035ac3578f97
Elia Innocenti <elia.innocenti@edu.unifi.it>
2024-12-17 10:27:59 +0100

    Refactor

--- compile.sh
+++ compile.sh
@@ -2,20 +2,41 @@
 
 # Define output directory
 OUTPUT_DIR="out"
+PDF_FILE='main.pdf'
 SECTIONS_DIR="sections"
+
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# Function to remove the main.pdf file in the root directory
+remove_root_pdf() {
+    if [ -f "$PDF_FILE" ]; then
+        rm -f "$PDF_FILE"
+        # printf "\n${GREEN}File ${PDF_FILE} removed from root directory.${RESET}\n"
+    else
+        printf "\n${YELLOW}File ${PDF_FILE} not found in root directory, nothing to remove.${RESET}\n"
+    fi
+}
 
 # Check for "clean" argument
 if [ "$1" = "clean" ]; then
     # Clean the output directory if it exists
     if [ -d "$OUTPUT_DIR" ]; then
         rm -rf "$OUTPUT_DIR"/*
-        echo "Output directory cleaned."
+        printf "\n${GREEN}Output directory cleaned.${RESET}\n"
     else
-        echo "Output directory does not exist, nothing to clean."
+        printf "\n${YELLOW}Output directory does not exist, nothing to clean.${RESET}\n"
     fi
+
+    # Remove main.pdf in the root directory
+    remove_root_pdf
 else
     # Create output directory if it doesn't exist
-    [ ! -d "$OUTPUT_DIR" ] && mkdir "$OUTPUT_DIR"
+    [ ! -d "$OUTPUT_DIR" ] && mkdir "$OUTPUT_DIR" && printf "\n${CYAN}Output directory created.${RESET}\n"
     
     # Check if "sections" directory exists and contains files
     if [ -d "$SECTIONS_DIR" ] && [ "$(ls -A $SECTIONS_DIR)" ]; then
@@ -31,13 +52,25 @@ else
             fi
         done
     fi
+    
+    printf "\n${CYAN}Compiling ...${RESET}\n"
 
     # Compile the LaTeX document (double compilation)
     pdflatex -output-directory="$OUTPUT_DIR" main.tex
     pdflatex -output-directory="$OUTPUT_DIR" main.tex
+    pdflatex -output-directory="$OUTPUT_DIR" main.tex
+    
+    # Check if the PDF file was generated successfully
+    if [ -f "$OUTPUT_DIR/$PDF_FILE" ]; then
+        # Remove any existing main.pdf in the root directory
+        remove_root_pdf
 
-    # Open the PDF automatically if on macOS (uncomment if desired)
-    # [ "$(uname)" = "Darwin" ] && open "$OUTPUT_DIR/main.pdf"
+        # Copy the PDF file to the root directory
+        cp "$OUTPUT_DIR/$PDF_FILE" ./
+        printf "\n${GREEN}File ${PDF_FILE} copied to root directory.${RESET}\n"
+    else
+        printf "\n${RED}Compilation failed. ${PDF_FILE} not found in ${OUTPUT_DIR}.${RESET}\n"
+    fi
 
-    echo "Compilation complete. Output available in $OUTPUT_DIR/"
+    printf "\n${GREEN}Compilation complete. Output available in ${CYAN}$OUTPUT_DIR/${RESET}\n"
 fi
